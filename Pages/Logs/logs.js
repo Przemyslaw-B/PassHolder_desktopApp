3e038e6b77ba43f30045d77da2e31ee797182576
Przemyslaw-B <przemyslaw_baczek@o2.pl>
2026-04-27 18:29:50 +0200

    Dodanie na froncie filtrowania listy logów.

--- Pages/Logs/logs.js
+++ Pages/Logs/logs.js
@@ -1,3 +1,9 @@
+  let logTypeFilter = "";
+  let logUserFilter = "";
+  let logSettedByFilter = "";
+
+  let loglterData;
+
 
   document.addEventListener("DOMContentLoaded", ()=>{
     const logsContainer = document.getElementById("logs-content");
@@ -5,13 +11,91 @@
       .then(res => res.text())
       .then(html => {
         logsContainer.innerHTML = html;
+        initFiltersSelector();
+        initFilterResetButtons();
         loadLogs();
       });
   });
 
+
+  function initFiltersSelector(){
+    const selectType = document.getElementById("logs-type-selector");
+    const typeResetWrapper = document.getElementById("type-logs-reset-icon");
+
+    const selectUser = document.getElementById("logs-user-selector");
+    const userResetWrapper = document.getElementById("user-logs-reset-icon");
+
+    const selectSettedBy = document.getElementById("logs-settedBy-selector");
+    const settedByResetWrapper = document.getElementById("settedBy-logs-reset-icon");
+
+    selectType.addEventListener('change', () =>{
+      logTypeFilter = selectType.value;
+      if(logTypeFilter && logTypeFilter !== ""){ 
+        typeResetWrapper.classList.add('active-filter');
+        selectType.classList.remove('active-filter-placeholder');
+      } else{
+        typeResetWrapper.classList.remove('active-filter');
+        selectType.classList.add('active-filter-placeholder');
+      }
+    });
+
+    selectUser.addEventListener('change', () =>{
+      logUserFilter = selectUser.value;
+      if(logUserFilter && logUserFilter !== ""){
+        userResetWrapper.classList.add('active-filter');
+        selectUser.classList.remove('active-filter-placeholder');
+      } else{
+        userResetWrapper.classList.remove('active-filter');
+        selectUser.classList.add('active-filter-placeholder');
+      }
+    });
+
+    selectSettedBy.addEventListener('change', () =>{
+      logSettedByFilter = selectSettedBy.value;
+      if(logSettedByFilter && logSettedByFilter !== ""){
+        settedByResetWrapper.classList.add('active-filter');
+        selectSettedBy.classList.remove('active-filter-placeholder');
+      } else{
+        settedByResetWrapper.classList.remove('active-filter');
+        selectSettedBy.classList.add('active-filter-placeholder');
+      }
+    });
+  } 
+
+  function initFilterResetButtons(){
+    const typeLogFilterResetButton = document.getElementById("type-logs-reset-icon");
+    const userLogFilterResetButton = document.getElementById("user-logs-reset-icon");
+    const settedByFilterResetButton = document.getElementById("settedBy-logs-reset-icon");
+  
+    typeLogFilterResetButton.addEventListener('click', ()=>{
+      const typeFilterSelector = document.getElementById("logs-type-selector");
+      typeFilterSelector.value = "";
+      typeLogFilterResetButton.classList.remove('active-filter');
+      typeFilterSelector.classList.add('active-filter-placeholder');
+    });
+
+    userLogFilterResetButton.addEventListener('click', ()=>{
+      const userFilterSelector = document.getElementById("logs-user-selector");
+      userFilterSelector.value="";
+      userLogFilterResetButton.classList.remove('active-filter');
+      userFilterSelector.classList.add('active-filter-placeholder');
+    });
+
+    settedByFilterResetButton.addEventListener('click', ()=>{
+      const settedByFilterSelector = document.getElementById("logs-settedBy-selector");
+      settedByFilterSelector.value="";
+      settedByFilterResetButton.classList.remove('active-filter');
+      settedByFilterSelector.classList.add('active-filter-placeholder');
+    });
+  }
+
   async function loadLogs(){
     try{
-      console.log("Ładowanie strony logów..");
+      //console.log("Ładowanie strony logów..");
+      logFilterData = await getFiltersData();
+      console.log("Filters data: ", logFilterData);
+      setTypeFiltersOptions();
+      setResponsibleAdministratorOptions();
       const data = await downloadLogsData();  //Pobierz listę logów
       console.log("data:", data);
       await setLogsGUI(data);
@@ -30,6 +114,33 @@
     return token;
   }
 
+  async function getFiltersData(){
+    try{
+      const responseConfig = await window.api.loadApiConfig();
+      const config = responseConfig.config;
+      const url = config.getFiltersData;
+      const token = await getToken();
+      if (!token) {
+        throw new Error("Brak tokenu");
+      }
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept': 'application/json'
+        }
+      });
+      if(!response.ok){
+        throw new Error(`Błąd API: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log("data filters:", data);
+      return data;
+    }catch(err){
+      console.error("[loadFiltersData] błąd:", err);
+    } 
+  }
+
   async function downloadLogsData(){
     try{
       const responseConfig = await window.api.loadApiConfig();  //odczytanie pliku endpointów
@@ -46,6 +157,7 @@
         },
         body: JSON.stringify({
           ammountOnPage: 0,
+          typeName: "",
           pageNumber: 0
         })
       });
@@ -61,7 +173,7 @@
   }
 
   async function setLogsGUI(data){
-    console.log("Data length: " + data.length)
+    //console.log("Data length: " + data.length)
     if(data != null && data.length > 0){
       const container = document.getElementById("log-list");
       container.innerHTML = ""; //Wyczyszczenie wierszy jeśli były
@@ -86,5 +198,43 @@
       }
     } else {
       console.log("Brak danych do wyświetlenia..");
+    }
+  }
+
+  function setTypeFiltersOptions(){
+    const events = logFilterData.events;
+    const select = document.getElementById("logs-type-selector");
+    select.replaceChildren();
+    const placeholder = new Option("Select type", "", true, true);
+    placeholder.disabled = true;
+    placeholder.hidden = true;
+    select.appendChild(placeholder);
+    if(events && events.length>0){
+      for(let event of events){
+        const option = document.createElement("option");
+        option.value = event.name;
+        option.textContent = event.name;
+        select.appendChild(option);
+      }
+    }
+  }
+
+  function setResponsibleAdministratorOptions(){
+    const admins = logFilterData.administrators;  
+    console.log("admins", admins);
+    const select = document.getElementById("logs-settedBy-selector");
+    select.replaceChildren();
+    const placeholder = new Option("Select administrator", "", true, true);
+    placeholder.disabled = true;
+    placeholder.hidden = true;
+    select.appendChild(placeholder);
+    if(admins && admins.length>0){
+      for(let admin of admins){
+        const option = document.createElement("option");
+        option.value = admin;
+        option.textContent = admin;
+        select.appendChild(option);
+        console.log("dodaję admina: ", admin);
+      }
     }
   }
